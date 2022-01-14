a89b0fd34f11ecb2b56f6c2677cf345294016d23
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-01-14 13:08:45 +0530

    Update phone book project.cpp

--- phone book project.cpp
+++ phone book project.cpp
@@ -1,6 +1,24 @@
 #include <bits/stdc++.h>
-#include <iostream>
+#include<ctype.h>
+#include<string.h>
+#include<windows.h>
+#include<conio.h>
+#include<stdio.h>
+#include<iomanip>
+#include<iostream>
+#include<dos.h>
+#include<fstream>
+#include<time.h>
+#include<ctype.h>
+#include<cstdio>
 using namespace std;
+COORD coord = {0, 0};
+void gotoxy (int x, int y)
+{
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
 
 class Phone
 {
@@ -31,6 +49,24 @@ void InsertAtHead(Phone *&head, string Value, long long int num)
     head = n;
 }
 
+bool IsCheck(pair<string, long long int> p1, pair<string, long long int> p2)
+{
+    if (p1.first > p2.first)
+    {
+        return false;
+    }
+    else if (p2.first > p1.first)
+    {
+        return true;
+    }
+    else
+    {
+        if (p1.second > p2.second)
+            return false;
+        else
+            return true;
+    }
+}
 void InsertNumber(Phone *&head, string Value, long long int num)
 {
 
@@ -50,44 +86,22 @@ void InsertNumber(Phone *&head, string Value, long long int num)
     n->prev = temp;
 }
 
-bool IsCheck(pair<string, long long int> p1, pair<string, long long int> p2)
-{
-    if (p1.first > p2.first)
-    {
-        return false;
-    }
-    else if (p2.first > p1.first)
-    {
-        return true;
-    }
-    else
-    {
-        if (p1.second > p2.second)
-            return false;
-        else
-            return true;
-    }
-}
+
+
 
 Phone *SortList(Phone *&head)
 {
-    multimap<string, long long int> mp;
+    // multimap<string, long long int> mp;
     vector<pair<string, long long int>> v;
     Phone *head2 = NULL;
 
     Phone *temp = head;
     while (temp != NULL)
     {
-        mp.insert(pair<string, long long int>(temp->Data, temp->Mobile));
+        v.push_back(pair<string, long long int>(temp->Data, temp->Mobile));
         temp = temp->next;
     }
-
-    for (auto &i : mp)
-    {
-        v.push_back(i);
-    }
     sort(v.begin(), v.end(), IsCheck);
-
     for (auto &it : v)
     {
         InsertNumber(head2, it.first, it.second);
@@ -187,7 +201,7 @@ bool Searching(Phone *head, long long int num)
         }
         if (result == true)
         {
-
+            Beep(450,600);
             cout << "\nName: " << temp->Data << " Mobile: " << temp->Mobile;
             Count++;
         }
@@ -197,33 +211,56 @@ bool Searching(Phone *head, long long int num)
     if (Count == 0)
     {
         cout << "\nContact Not Present.";
+        Beep(190,300);
     }
 
-    // return result;
+
 }
 
 bool SearchByName(Phone *head, string Name)
 {
     Phone *temp = head;
+    Phone *temp2 = head;
+    int count3 = 0;
+
     bool Search = false;
 
     while (temp != NULL)
     {
         if (temp->Data == Name)
         {
+            Beep(235, 540);
             cout << "\nName: " << temp->Data << " Mobile: " << temp->Mobile;
             Search = true;
+            count3++;
         }
 
         temp = temp->next;
     }
+    if (count3 == 0)
+    {
+        while (temp2 != NULL)
+        {
+            if (temp2->Data.find(Name) != string::npos) ///string::npos is an inbuilt function which has value -1.
+            {
+                Beep(235, 540);
+
+                cout << "\nName: " << temp2->Data << " Mobile: " << temp2->Mobile;
+                Search = true;
+            }
+
+            temp2 = temp2->next;
+        }
+    }
+
     if (Search == false)
     {
         cout << "\nContact Not Present.";
+        Beep(350, 500);
     }
-
     return Search;
 }
+
 
 void UpdateContact(Phone *&head, long long int num)
 {
@@ -324,7 +361,7 @@ void UpdateContact(Phone *&head, long long int num)
 
                 cout << "Enter new Phone Number: ";
                 cin >> mob;
-
+                Beep(200,600);
                 temp->Data = Name;
                 temp->Mobile = mob;
             }
@@ -337,8 +374,10 @@ void UpdateContact(Phone *&head, long long int num)
     if (Count == 0)
     {
         cout << "\nContact Not Present.";
+        Beep(345,710);
     }
 }
+
 
 void UpdateByName(Phone *&head, string Name)
 {
@@ -359,6 +398,7 @@ void UpdateByName(Phone *&head, string Name)
             cout << "\n\nDo you want to update this record: \nPress 0 for No and Press 1 for Yes";
             cout << "\nEnter your choice: ";
             cin >> choice;
+            Beep(234, 550);
 
             if (choice == 1)
             {
@@ -381,28 +421,81 @@ void UpdateByName(Phone *&head, string Name)
     if (Search == false)
     {
         cout << "\nContact Not Present.";
+        Beep(350, 500);
     }
 }
 
+Phone *deletenode(Phone *head, string s, long long int n)
+{
+    Phone *curr = head;
+    while (curr != NULL and curr->Data != s && curr->Mobile != n)
+    {
+        curr = curr->next;
+    }
+    if (curr == head)
+    {
+        curr->next->prev = NULL;
+        head = head->next;
+        delete curr;
+        return head;
+    }
+    else
+    {
+        if (curr->next != NULL || curr->prev != NULL)
+        {
+            curr->prev->next = curr->next;
+            curr->next->prev = curr->prev;
+            curr = curr->next;
+            delete curr;
+            return head;
+        }
+    }
+}
+
+Phone *displaydelete_information(Phone *head, string x)
+{
+    if (head != NULL)
+    {
+        Phone *curr = head;
+
+        while (curr != NULL and curr->Data != x)
+            curr = curr->next;
+        while (curr != NULL and curr->Data == x)
+        {
+            cout << curr->Data << " " << curr->Mobile << endl;
+            curr = curr->next;
+        }
+
+        long long int pn;
+        string s;
+        cout << "enter the phone number and name which you want to delete" << endl;
+        cin >> pn;
+        fflush(stdin);
+        getline(cin, s);
+        fflush(stdin);
+        head = deletenode(head, s, pn);
+    }
+
+    return head;
+}
 void Display(Phone *head)
 {
     head = SortList(head);
     Phone *n = head;
-
     if (n == NULL)
         cout << "\nNO RECORDS PRESENT.\n";
     else
     {
-        cout << "\n****DISPLAYING DETAILS**\n";
+        cout << "\n*******DISPLAYING DETAILS*******\n";
         while (n != NULL)
         {
             cout << " Name: " << n->Data << " Mobile: " << n->Mobile << "\n";
             n = n->next;
+            Beep(245,550);
         }
-        cout << "\n************\n";
+        cout << "\n********************************\n";
     }
 }
-
 void AddNumber(Phone *&head)
 {
     string Name;
@@ -415,27 +508,151 @@ void AddNumber(Phone *&head)
     cin >> num;
     InsertNumber(head, Name, num);
 }
+void start()
+{
 
+    int a;
+    system("color 57");
+    Beep(350,700);
+    Beep(200,800);
+    Beep(400,250);
+    Beep(523,600);
+    Beep(540,600);
+    for(a=50; a>30; a--)
+    {
+        gotoxy(25,10);
+        cout<<"Welcome to the PHONEBOOK MANAGEMNET SYSTEM";
+        Sleep(50);
+    }
+    for(a=42; a<52; a++)
+    {
+        gotoxy(27,18);
+        cout<<"Please Wait";
+        gotoxy(a,18);
+        cout<<".";
+        Sleep(200);
+    }
+    system("cls");
+    system("color A0");
+    for(int j=0; j<10; j++)
+    {
+        system("cls");
+        gotoxy(j,6);
+        cout<<"   ����������������������������";
+        gotoxy(j,7);
+        cout<<"      ������   �    �   ���������";
+        gotoxy(j,8);
+        cout<<"      �    �   �    �   �       �";
+        gotoxy(j,9);
+        cout<<"      �    �   �    �   �       �";
+        gotoxy(j,10);
+        cout<<"      ������   ������   �       �";
+        gotoxy(j,11);
+        cout<<"      �        �    �   �       �";
+        gotoxy(j,12);
+        cout<<"      �        �    �   �       �";
+        gotoxy(j,13);
+        cout<<"      �        �    �   ���������";
+
+
+        gotoxy(51-j,6);
+        cout<<"���������������������    \n\r";
+        gotoxy(51-j,7);
+        cout<<" �      �   ��������      \n\r";
+        gotoxy(51-j,8);
+        cout<<" ��     �   �               \n\r";
+        gotoxy(51-j,9);
+        cout<<" � �    �   �            \n\r";
+        gotoxy(51-j,10);
+        cout<<" �  �   �   ��������      \n\r";
+        gotoxy(51-j,11);
+        cout<<" �    � �   �             \n\r\n\n";
+        gotoxy(51-j,12);
+        cout<<" �     ��   �             \n\r";
+        gotoxy(51-j,13);
+        cout<<" �      �   ��������       \n\r\n\n\n\n\n\n\n";
+
+        gotoxy(j,16);
+        cout<<"                    ���������  ���������";
+        gotoxy(j,17);
+        cout<<"                    �       �  �       �";
+        gotoxy(j,18);
+        cout<<"                    �       �  �       �";
+        gotoxy(j,19);
+        cout<<"                    ���������  �       �";
+        gotoxy(j,20);
+        cout<<"                    �       �  �       �";
+        gotoxy(j,21);
+        cout<<"                    �       �  �       �";
+        gotoxy(j,22);
+        cout<<"                    ���������  ���������";
+        gotoxy(j,23);
+        cout<<"                    �������������������������";
+
+        gotoxy(58-j,16);
+        cout<<" ��������    �   �        \n\r";
+        gotoxy(58-j,17);
+        cout<<" �      �    �  �          \n\r";
+        gotoxy(58-j,18);
+        cout<<" �      �    � �           \n\r";
+        gotoxy(58-j,19);
+        cout<<" �      �    ��             \n\r";
+        gotoxy(58-j,20);
+        cout<<" �      �    � �            \n\r";
+        gotoxy(58-j,21);
+        cout<<" �      �    �  �           \n\r\n\n";
+        gotoxy(58-j,22);
+        cout<<" ��������    �   �          \n\r";
+        gotoxy(58-j,23);
+        cout<<" ����������������������        \n\r";
+
+
+
+        Sleep(80);
+    }
+    gotoxy(25,25);
+    cout<<"press any key to continue...";
+    Beep(250,450);
+    getch();
+    Sleep(50);
+
+}
 int main()
 {
+
     Phone *head = NULL;
     int i = 1, choice, choice2;
     string Name;
     long long int num;
 
+    start();
+
+    system("cls");
+
     while (i)
     {
-        cout << "\n\n******************************************************************************************";
+        system("cls");
+
+        cout << "\n\n******************************";
         cout << "\n\t\t\tWELCOME TO PHONEBOOK MANAGEMENT SYSTEM\n";
-        cout << "******************************************************************************************\n";
+        cout << "******************************\n";
         cout << "\nPress 1 to Insert Number ";
+        Beep(350, 700);
         cout << "\nPress 2 to Search Number ";
+        Beep(400, 700);
         cout << "\nPress 3 to Update Number ";
+        Beep(450, 700);
         cout << "\nPress 4 to Display PhoneBook";
+        Beep(250, 700);
         cout << "\nPress 5 to Count Total Records";
-        cout << "\nPress 6 to Exit";
+        Beep(200, 700);
+        cout << "\nPress 6 to Delete Number";
+        Beep(200, 700);
+        cout << "\nPress 7 to Exit";
+        Beep(150, 700);
         cout << "\nEnter your choice: ";
         cin >> choice;
+        Beep(234, 550);
 
         switch (choice)
         {
@@ -444,6 +661,7 @@ int main()
 
             AddNumber(head);
             cout << "\nNumber Added Successfully.\n";
+            Beep(234, 550);
         }
 
         break;
@@ -453,8 +671,10 @@ int main()
             cout << "\n Press 1 to Search by Name ";
             cout << "\n Press 2 to Search by Phone Number ";
             cout << "\nEnter your choice: ";
+            SortList(head);
             int Schoice;
             cin >> Schoice;
+            Beep(200, 400);
 
             if (Schoice == 1)
             {
@@ -479,10 +699,16 @@ int main()
         case 3:
         {
             cout << "\n Press 1 to Update by Name ";
+            Beep(450, 500);
+            Sleep(100);
             cout << "\n Press 2 to Update by Phone Number ";
+            Beep(350, 400);
             cout << "\nEnter your choice: ";
             int Uchoice;
             cin >> Uchoice;
+            Beep(350, 500);
+
+            SortList(head);
 
             if (Uchoice == 1)
             {
@@ -499,11 +725,11 @@ int main()
                 cin >> num;
                 UpdateContact(head, num);
             }
-            cout<<"\n Number Updated Successfully ";
         }
         break;
         case 4:
         {
+            SortList(head);
             Display(head);
             break;
         }
@@ -516,14 +742,26 @@ int main()
         break;
         case 6:
         {
+            cout << "\nEnter the name you want to Delete: ";
+            fflush(stdin);
+            getline(cin, Name);
+            fflush(stdin);
+            head = displaydelete_information(head, Name);
+            Display(head);
+        }
+        break;
+        case 7:
+        {
             exit(0);
         }
-
         default:
         {
             cout << "\nWrong Choice Entered.Enter Again.\n";
         }
         break;
         }
+        cout << "enter any key to go back to menu";
+        getch();
+        system("cls");
     }
 }
